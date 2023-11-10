6923ab27b4c2bd6465ee6618d44c4468c7794aa7
akopij70 <a.kopij70@gmail.com>
2023-11-10 22:38:31 +0100

    Dodanie strony kontaktowej. Added contact page.

--- dkf/core/urls.py
+++ dkf/core/urls.py
@@ -7,6 +7,7 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('contact', views.contact, name='contact'),
     path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='core/logout.html', next_page=None), name='logout'),
     path('signup/', views.signup, name='signup'),
